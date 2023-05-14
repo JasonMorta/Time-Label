90966080a7f7c3bc9fe25dfb1d1e24bcdb84f11a
Jason M <jason.morta23@gmail.com>
2023-05-14 16:10:21 +0200

    updates

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 
 let page = document.getElementsByTagName("body")[0];
 let clock = document.createElement("div");
-clock.className = "CL-clock";
+clock.className = "CL-clock ";
+clock.classList.add("CL-slide-in-top");
 page.appendChild(clock);
 let startTimer = setInterval(updateTime, 1000);
 
@@ -37,7 +38,7 @@ async function handleVisibilityChange() {
 function updateTime() {
     const now = new Date();
     const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-    console.log('options', options)
+   
     const time = now.toLocaleTimeString([], options);
   clock.innerHTML = '<p class="CL-innerText">' + time.replace(/(\d+:\d+):(\d+)/, '$1<span class="CL-seconds">$2</span>') + '</p>'; ;
 }
